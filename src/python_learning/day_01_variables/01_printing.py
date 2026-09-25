a8187bebb45f714c365e3fd2add8dca7fed3bf8d
4h4cayj <87427092+4H4CayJ@users.noreply.github.com>
2026-09-25 10:48:39 +0000

    file name update python_learning to python

--- src/python_learning/day_01_variables/01_printing.py
+++
@@ -1,9 +0,0 @@
-"""
-Day: 1
-Concept: print function
-Summary: Learning how to use print function in Python.
-"""
-
-# print function is used to display output to the console.
-
-print("Hello, World!")
